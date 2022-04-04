28c9e0b2b335689ec99d94fe8fee0ea78ca965e2
dna1392 <carp072016@outlook.es>
2022-04-04 11:22:00 -0500

    Recovered and boosted to 48%

--- login.php
+++ login.php
@@ -3,8 +3,8 @@
 </head>
 <body>
     <?php include_once 'includes/navbar.php'; ?>
-    <div class="container">
-        <br><br>
+    <div class="login">
+        <br>
         <h2>Iniciar sesión</h2><br>
         <form>
             <div class="mb-3">
@@ -15,13 +15,16 @@
                 <label for="password" class="form-label">Contraseña</label>
                 <input type="password" class="form-control" id="password">
             </div>
-            <div class="mb-3 form-check">
-                <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                <label class="form-check-label" for="exampleCheck1">Check me out</label>
-            </div>
-            <button type="submit" class="btn btn-primary">Iniciar sesión</button>
+            <br>
+            <button type="submit" class="btn btn-primary">Ingresar</button>
         </form>
     </div>
+    <div class="banner"></div>
+    <footer id="sticky-footer" class="flex-shrink-0 py-4 bg-dark text-white-50">
+    <div class="container text-center">
+      <small>Copyright &copy; Your Website</small>
+    </div>
+  </footer>
 </body>
 
 </html>
